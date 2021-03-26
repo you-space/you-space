e56bfbb25f07f533b9c89def9127d399531c82f0
Henrique <henriqueoliwork@gmail.com>
2021-03-26 18:09:15 -0300

    dashboard: fix admin routes

--- start/routes/admin.ts
+++ start/routes/admin.ts
@@ -1,17 +1,17 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/login', 'DashboardController.login')
-
 Route.group(() => {
-  // Route.get('/', 'DashboardController.show')
+  Route.get('/', 'DashboardController.show')
+  Route.get('*', 'DashboardController.show')
 }).prefix('ys-admin')
 
 Route.group(() => {
-  Route.patch('videos/update-all', 'AdminVideosController.updateAll')
   Route.resource('videos', 'AdminVideosController').apiOnly()
+  Route.patch('videos/update-all', 'AdminVideosController.updateAll')
   Route.resource('origins', 'AdminOriginsController').apiOnly()
 })
   .prefix('admin')
   .prefix('v1')
+  .prefix('api')
 // .middleware('auth:api')
 // .middleware('acl:admin')
---
+++ start/routes/videos.ts
@@ -0,0 +1,18 @@
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.group(() => {
+  Route.post('/sign-up', 'AuthController.store')
+  Route.post('/login', 'AuthController.login')
+  Route.get('/who-i-am', 'AuthController.show')
+
+  Route.get('videos', 'VideosController.index')
+  Route.get('videos/trending', 'VideosController.trending')
+  Route.get('videos/:id', 'VideosController.show')
+
+  Route.get('comments/:videoId', 'CommentsController.showVideoComments')
+
+  Route.get('videos/embed/:id', 'FilesController.embed')
+  Route.get('files/embed/:id', 'FilesController.showFile')
+})
+  .prefix('v1')
+  .prefix('api')
