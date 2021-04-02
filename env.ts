618625fd30e97d4357da8e490208d30c3f362b11
Henrique <henryqueoli@hotmail.com>
2021-04-02 20:17:10 -0300

    adonis: fix env variables schema

--- env.ts
+++ env.ts
@@ -15,10 +15,9 @@
 import Env from '@ioc:Adonis/Core/Env'
 
 export default Env.rules({
-  HOST: Env.schema.string({ format: 'host' }),
-  PORT: Env.schema.number(),
   APP_KEY: Env.schema.string(),
   APP_NAME: Env.schema.string(),
   NODE_ENV: Env.schema.enum(['development', 'production', 'testing'] as const),
   DOMAIN_URL: Env.schema.string(),
+  POSTGRES_URL: Env.schema.string(),
 })
