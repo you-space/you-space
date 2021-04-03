59339e7a4758e076cef3db3f474cb77860407e4f
Henrique <henryqueoli@hotmail.com>
2021-04-02 22:05:47 -0300

    origins: add update methods in dashboard

--- public/src/pages/OriginList.vue
+++ public/src/pages/OriginList.vue
@@ -1,32 +1,48 @@
 <template>
-  <q-page class="row items-start q-pa-lg">
-    <q-table 
-      class="full-width"
-      :columns="columns"
-      :rows="rows"
-      :rows-per-page-options="[10, 20, 0]"
-    >
-      <template #top-right>
-        <q-btn @click="dialog = true">
-          {{ $t('addNew') }}
-        </q-btn>
-      </template>
-     
-      <template #body-cell-actions="props">
-        <q-td :props="props">
-          <q-icon
-            name="delete"
-            @click="deleteOrigin(props.row.id)"
-          />
-        </q-td>
-      </template>
-    </q-table>
-    
-    <origin-list-dialog
-      v-model="dialog"
-      @save="setOrigins"
-    />
-  </q-page>
+    <q-page class="row items-start q-pa-lg">
+        <q-table
+            class="full-width"
+            :columns="columns"
+            :rows="rows"
+            :rows-per-page-options="[10, 20, 0]"
+        >
+            <template #top-right>
+                <q-btn
+                    @click="
+                        editedItemid = null;
+                        dialog = true;
+                    "
+                >
+                    {{ $t('addNew') }}
+                </q-btn>
+            </template>
+
+            <template #body-cell-actions="props">
+                <q-td :props="props">
+                    <q-btn
+                        icon="edit"
+                        size="sm"
+                        flat
+                        round
+                        @click="editOrigin(props.row.id)"
+                    />
+                    <q-btn
+                        icon="delete"
+                        size="sm"
+                        flat
+                        round
+                        @click="deleteOrigin(props.row.id)"
+                    />
+                </q-td>
+            </template>
+        </q-table>
+
+        <origin-list-dialog
+            v-model="dialog"
+            :origin-id="editedItemid"
+            @save="setOrigins"
+        />
+    </q-page>
 </template>
 
 <script lang="ts">
@@ -34,17 +50,21 @@ import lodash from 'lodash';
 import { defineComponent, ref, defineAsyncComponent, onMounted } from 'vue';
 import { useI18n } from 'vue-i18n';
 
-import { api  } from 'boot/axios';
+import { api } from 'boot/axios';
 
 export default defineComponent({
     components: {
-        OriginListDialog: defineAsyncComponent(() => import('./OriginListDialog.vue'))
+        OriginListDialog: defineAsyncComponent(
+            () => import('./OriginListDialog.vue'),
+        ),
     },
     setup() {
         const tm = useI18n();
-        
+
         const rows = ref<any[]>([]);
         const dialog = ref(false);
+
+        const editedItemid = ref<number | null>(null);
 
         const columns = [
             {
@@ -52,50 +72,56 @@ export default defineComponent({
                 name: 'name',
                 field: 'name',
                 align: 'left',
-                style: 'width:100px'
+                style: 'width:100px',
             },
             {
                 label: tm.t('type'),
                 name: 'type',
                 field: 'type',
-                align: 'left'
+                align: 'left',
             },
             {
                 label: tm.t('totalVideos'),
                 name: 'totalVideos',
                 field: (row: any) => lodash.get(row, 'metadata.totalVideos', 0),
-                align: 'left'
+                align: 'left',
             },
             {
                 label: tm.t('registeredVideos'),
                 name: 'totalVideos',
-                field: (row: any) => lodash.get(row, 'metadata.registeredVideos', 0),
-                align: 'left'
+                field: (row: any) =>
+                    lodash.get(row, 'metadata.registeredVideos', 0),
+                align: 'left',
             },
-            {
-                name: 'actions', 
-            },
+            { name: 'actions' },
         ];
 
         const setOrigins = async () => {
-            const {data } = await api.get('/admin/origins');
+            const { data } = await api.get('/admin/origins');
             rows.value = data;
         };
-        
+
+        const editOrigin = (id: number) => {
+            editedItemid.value = id;
+            dialog.value = true;
+        };
+
         const deleteOrigin = async (id: number) => {
             await api.delete(`/admin/origins/${id}`);
             await setOrigins();
         };
 
         onMounted(setOrigins);
-    
+
         return {
             rows,
             columns,
             dialog,
             deleteOrigin,
-            setOrigins
+            setOrigins,
+            editOrigin,
+            editedItemid,
         };
-    }
+    },
 });
 </script>
--- public/src/pages/OriginListDialog.vue
+++ public/src/pages/OriginListDialog.vue
@@ -1,90 +1,115 @@
 <template>
-  <q-dialog v-model="dialog">
-    <q-card
-      class="full-width flex"
-      style="max-width:500px"
-    >
-      <q-card-section class="full-width">
-        <q-input
-          v-model="name"
-          :label="$t('name')"
-          class="q-mb-sm"
-        />
-        <q-select
-          v-model="type"
-          :label="$t('type')"
-          class="q-mb-sm"
-          :options="types"
-        />
-        <template v-if="type === 'you-tube'">
-          <q-input            
-            v-model="config.apiKey"
-            :label="$t('apiKey')"
-          />
-          
-          <q-input            
-            v-model="config.channelId"
-            :label="$t('channelId')"
-          />
-        </template>
-      </q-card-section>
+    <q-dialog v-model="dialog">
+        <q-card class="full-width flex" style="max-width: 500px">
+            <q-card-section class="full-width">
+                <q-input v-model="name" :label="$t('name')" class="q-mb-sm" />
+                <q-select
+                    v-model="type"
+                    :label="$t('type')"
+                    class="q-mb-sm"
+                    :options="types"
+                />
+                <template v-if="type === 'you-tube'">
+                    <q-input v-model="config.apiKey" :label="$t('apiKey')" />
 
-      <q-card-section class="text-right full-width">
-        <q-btn
-          :disabled="!name || !type"
-          color="primary"
-          class="q-mr-sm" 
-          @click="saveOrigin"
-        >
-          {{ $t("save") }}
-        </q-btn>
-        <q-btn @click="dialog = false">
-          {{ $t("cancel") }}
-        </q-btn>
-      </q-card-section>
-    </q-card>
-  </q-dialog>
+                    <q-input
+                        v-model="config.channelId"
+                        :label="$t('channelId')"
+                    />
+                </template>
+            </q-card-section>
+
+            <q-card-section class="text-right full-width">
+                <q-btn
+                    :disabled="!name || !type"
+                    color="primary"
+                    class="q-mr-sm"
+                    @click="saveOrigin"
+                >
+                    {{ $t('save') }}
+                </q-btn>
+                <q-btn @click="dialog = false">
+                    {{ $t('cancel') }}
+                </q-btn>
+            </q-card-section>
+        </q-card>
+    </q-dialog>
 </template>
 
-<script lang='ts' >
+<script lang="ts">
 import { api } from 'src/boot/axios';
-import { defineComponent, ref, computed } from 'vue';
+import { defineComponent, ref, computed, watch } from 'vue';
 export default defineComponent({
     props: {
         modelValue: {
             type: Boolean,
             default: false,
-        }
+        },
+        originId: {
+            type: Number,
+            required: false,
+            default: null,
+        },
     },
     emits: ['update:modelValue', 'save'],
-    setup(props, { emit }){
+    setup(props, { emit }) {
         const name = ref('');
         const type = ref('');
-        const config = ref({
+        const config = ref({});
 
-        });
-        
-        const types = [
-            'you-tube',
-            'twitch'
-        ];
+        const types = ['you-tube', 'twitch'];
 
         const dialog = computed({
-            get(){
+            get() {
                 return props.modelValue;
             },
-            set(value){
+            set(value) {
                 emit('update:modelValue', value);
-            }
+            },
         });
 
-        const saveOrigin = async () => {
+        const setOrigin = async () => {
+            if (!props.originId) {
+                name.value = '';
+                type.value = '';
+                config.value = {};
+                return;
+            }
 
+            const { data } = await api.get(`admin/origins/${props.originId}`);
+            name.value = data.name;
+            type.value = data.type;
+            config.value = data.config;
+        };
+
+        if (props.originId) {
+            void setOrigin();
+        }
+
+        watch(() => props.originId, setOrigin);
+
+        const createOrigin = async () => {
             await api.post('admin/origins', {
                 name: name.value,
                 type: type.value,
-                config: config.value
+                config: config.value,
             });
+        };
+
+        const updateOrigin = async () => {
+            await api.patch(`admin/origins/${props.originId}`, {
+                name: name.value,
+                type: type.value,
+                config: config.value,
+            });
+        };
+
+        const saveOrigin = async () => {
+            if (props.originId) {
+                await updateOrigin();
+            } else {
+                await createOrigin();
+            }
 
             emit('save');
 
@@ -97,12 +122,10 @@ export default defineComponent({
             name,
             type,
             types,
-            config
+            config,
         };
-    }
+    },
 });
 </script>
 
-<style>
-
-</style>
+<style></style>
