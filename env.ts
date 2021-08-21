cf09bf167882225b73835ca1c98670bf6d121991
Henrique <henryqueoli@hotmail.com>
2021-08-21 03:51:04 -0300

    fix: set APP_KEY on startup if is not defined

--- env.ts
+++ env.ts
@@ -1,18 +1,13 @@
-/*
-|--------------------------------------------------------------------------
-| Validating Environment Variables
-|--------------------------------------------------------------------------
-|
-| In this file we define the rules for validating environment variables.
-| By performing validation we ensure that your application is running in
-| a stable environment with correct configuration values.
-|
-| This file is read automatically by the framework during the boot lifecycle
-| and hence do not rename or move this file to a different location.
-|
-*/
-
 import Env from '@ioc:Adonis/Core/Env'
+import { string } from '@ioc:Adonis/Core/Helpers'
+
+if (!process.env.APP_KEY) {
+  process.env.APP_KEY = string.generateRandom(32)
+}
+
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = 'production'
+}
 
 const rules = {
   NODE_ENV: Env.schema.enum(['development', 'production', 'testing'] as const),
