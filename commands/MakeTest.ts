9dbb06f120c87d80fa61a2c6f51840eca0625313
Henrique <henryqueoli@hotmail.com>
2021-10-12 09:56:58 -0300

    refactor: remake make:test ace command

--- commands/MakeTest.ts
+++ commands/MakeTest.ts
@@ -1,9 +1,9 @@
 import { BaseCommand, args } from '@adonisjs/core/build/standalone'
-import { dirname, basename, join } from 'path'
+import { join } from 'path'
 
 export default class MakeUnitTest extends BaseCommand {
   public static commandName = 'make:test'
-  public static description = ''
+  public static description = 'Create a test file'
 
   public static settings = {
     loadApp: false,
@@ -11,17 +11,17 @@ export default class MakeUnitTest extends BaseCommand {
   }
 
   @args.string({ description: 'Source file to create teste', required: true })
-  public filename: string
+  public name: string
 
   public async run() {
     this.generator
-      .addFile(basename(this.filename), { extname: '.unit.test.ts' })
+      .addFile(this.name, { extname: '.test.ts', pattern: 'pascalcase' })
       .appRoot(this.application.appRoot)
-      .destinationDir(dirname(this.filename))
+      .destinationDir('tests')
       .useMustache()
       .stub(join(__dirname, './templates/test.txt'))
       .apply({
-        name: `${basename(this.filename)} (unit)`,
+        name: this.name,
       })
 
     await this.generator.run()
