7a5dfd9b15431352fc8ceae2b83c244087a92c89
Henrique <henryqueoli@hotmail.com>
2021-10-23 14:30:47 -0300

    feat: add theme:index and theme:activate events

--- app/Listeners/ThemeListener.ts
+++ app/Listeners/ThemeListener.ts
@@ -1,11 +1,26 @@
+import fs from 'fs'
 import Application from '@ioc:Adonis/Core/Application'
 import Drive from '@ioc:Adonis/Core/Drive'
+import Space from 'App/Services/SpaceService'
+import SystemMeta, { SystemDefaults } from 'App/Models/SystemMeta'
 
 export interface Theme {
   filename: string
 }
 
 export default class ThemeListener {
+  public async index() {
+    const folders = await fs.promises.readdir(Application.makePath('content', 'themes'), {
+      withFileTypes: true,
+    })
+
+    return folders
+      .filter((f) => f.isDirectory())
+      .map((t) => ({
+        name: t.name,
+      }))
+  }
+
   public async show(name: string) {
     const filename = Application.makePath('content', 'themes', name, 'index.js')
 
@@ -19,5 +34,25 @@ export default class ThemeListener {
       filename,
       name,
     }
+  }
+
+  public async activate(name: string) {
+    const theme = await this.show(name)
+
+    const { start } = await import(theme.filename)
+
+    if (start) {
+      await start()
+    }
+
+    await SystemMeta.updateOrCreate(
+      {
+        name: SystemDefaults.CurrentTheme,
+      },
+      {
+        name: SystemDefaults.CurrentTheme,
+        value: name,
+      }
+    )
   }
 }
