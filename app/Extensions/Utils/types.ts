e903693cdfc2e8817e876dcdc887652e443a752a
Henrique <henryqueoli@hotmail.com>
2021-10-27 17:34:36 -0300

    refactor: remove old code

--- app/Extensions/Utils/types.ts
+++
@@ -1,8 +0,0 @@
-export type This<T extends new (...args: any) => any> = {
-  new (...args: ConstructorParameters<T>): any
-} & Pick<T, keyof T>
-
-export interface HookListener {
-  event: string
-  handler: (data: any) => Promise<void>
-}
