bf2415820acacba61cf10eda99e9a8f92a89da15
Henrique <henryqueoli@hotmail.com>
2021-10-29 01:31:14 -0300

    feat: add images model and migration

---
+++ database/migrations/1635479150900_images.ts
@@ -0,0 +1,21 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Images extends BaseSchema {
+  protected tableName = 'images'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.string('src').notNullable()
+      table.string('alt')
+
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
