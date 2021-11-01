91099fd01fc3b1614ce158d7310b5691be92d65a
Henrique <henryqueoli@hotmail.com>
2021-11-01 09:22:01 -0300

    feat: add comment model and migration

---
+++ database/migrations/1635768103490_comments.ts
@@ -0,0 +1,30 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Comments extends BaseSchema {
+  protected tableName = 'comments'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.string('source_id')
+      table.string('source').notNullable().defaultTo('unknown')
+
+      table.integer('parent_id').references('comments.id')
+      table.integer('video_id').notNullable().references('videos.id').onDelete('CASCADE')
+
+      table.string('avatar_src')
+      table.string('username').notNullable()
+      table.string('content').notNullable()
+
+      table.dateTime('published_at').notNullable()
+
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
